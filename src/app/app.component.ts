37608617072504333a901d8a3de2948be7c08cd4
Garza Rodriguez Gerardo <gerardo_cosmo@hotmail.com>
2022-05-26 18:24:45 -0500

    Primera carga

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //  title:string = 'Contador';
+//   contador: number = 0;
+//   base: number =10;
+
+//   // sumar(){
+//   //   this.contador++;
+//   // }
+//   // restar(){
+//   //   this.contador--;
+//   // }
+// acumular(valor: number){
+// this.contador += valor;
+// }
+
+}
